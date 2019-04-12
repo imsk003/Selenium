d098b0461e52e5bbf86d7979323cc992c6ea439a
SelvaKumar T <selvamailz@gmail.com>
2019-04-12 15:22:30 +0530

    updated css selector

--- LearningLocators/src/sk/learning/locators/Locators.java
+++ LearningLocators/src/sk/learning/locators/Locators.java
@@ -26,7 +26,7 @@ public class Locators {
 		
 //		identifying element by its css path
 //		clicking on login button
-		driver.findElement(By.cssSelector("html#facebook body.fbIndex.UIPage_LoggedOut._-kb._61s0._605a.b_c3pyn-ahh.gecko.win.x1.Locale_en_GB.cores-lt4._19_u.hasAXNavMenubar div#u_0_b._li div#pagelet_bluebar div#blueBarDOMInspector div._53jh div.loggedout_menubar_container div.clearfix.loggedout_menubar div.menu_login_container.rfloat._ohf form#login_form table tbody tr td label#loginbutton.uiButton.uiButtonConfirm input#u_0_2")).click();
+		driver.findElement(By.cssSelector("input#u_0_2")).click();
 		
 //		identifying element by its linkText
 //		clicking on Forgotten account? link
